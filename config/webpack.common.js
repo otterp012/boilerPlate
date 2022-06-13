14749548f11a223ad06c72fc790d5f82fe2a0953
otterp012 <otterp012@gmail.com>
2022-06-13 13:53:47 +0900

    add boilerplate

---
+++ config/webpack.common.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: `${path.resolve(__dirname, '../src')}/index.tsx`,
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${path.resolve(__dirname, '../public')}/index.html`,
+    }),
+    new webpack.ProvidePlugin({}),
+    new Dotenv(),
+  ],
+  resolve: {
+    modules: [path.resolve(__dirname, '../src'), 'node_modules'],
+
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    alias: {
+      '@UI': path.resolve(__dirname, '../src/UI'),
+      '@components': path.resolve(__dirname, '../src/components'),
+    },
+  },
+};
